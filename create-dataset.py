2fa6ff723e3400bf3462879bd98bd7a634421b2a
fahimahmed0x <cpcheats1991@gmail.com>
2021-12-29 13:38:41 -0500

    Webscrape each URL in the recently sold page, and add to dataset. Also add checks to prevent errors when finding sale price, offer price, and bid price.

--- create-dataset.py
+++ create-dataset.py
@@ -32,54 +32,64 @@ sales = []
 offers = []
 bids = []
 
-#try working with most recently sold CryptoPunk
-request = requests.get(base_url + sale_links[0])
-soup = BeautifulSoup(request.text)
-print(soup.title.string)
+# for each CryptoPunk listed in the recently sold page
+for sale_link in sale_links:
+    request = requests.get(base_url + sale_link)
+    soup = BeautifulSoup(request.text)
+    print(soup.title.string)
 
-# ID and type
-details = soup.find("div", class_="col-md-10 col-md-offset-1 col-xs-12")
-id = details.h1.text
-type = details.h4.a.text
+    # ID and type
+    details = soup.find("div", class_="col-md-10 col-md-offset-1 col-xs-12")
+    id = details.h1.text
+    type = details.h4.a.text
 
-#attributes
-details = soup.find("div", class_="col-md-10 col-md-offset-1")
-attributeCount = details.p.a.text # number of attributes as a String
-attributeCount = attributeCount.split(' ')[0] # get just the number in the sentence
+    #attributes
+    details = soup.find("div", class_="col-md-10 col-md-offset-1")
+    attributeCount = details.p.a.text # number of attributes as a String
+    attributeCount = attributeCount.split(' ')[0] # get just the number in the sentence
 
-punkAttributes = [] #attributes specific to this CryptoPunk
-#append attributes to a list
-for attribute in soup.find_all('div', class_="col-md-4"):
-    punkAttributes.append(attribute.a.text)
-punkAttributes = punkAttributes[1:] #ignore the 0th index as it's not an attribute.
+    punkAttributes = [] #attributes specific to this CryptoPunk
+    #append attributes to a list
+    for attribute in soup.find_all('div', class_="col-md-4"):
+        punkAttributes.append(attribute.a.text)
+    punkAttributes = punkAttributes[1:] #ignore the 0th index as it's not an attribute.
 
-#checking
-if int(attributeCount) == len(punkAttributes):
-    print('success!')
+    #checking
+    if int(attributeCount) == len(punkAttributes):
+        print('success!')
 
-# most recent sale price
-# https://stackoverflow.com/questions/64542519/getting-first-or-a-specific-td-in-beautifulsoup-with-no-class
-# foruth column represents price, and we want the most recent price so select 0th index. alternatively, use select_one
-details = soup.select("tr.punk-history-row-sold td:nth-of-type(4)")[0]
-salePrice = details.text
+    # most recent sale price
+    # https://stackoverflow.com/questions/64542519/getting-first-or-a-specific-td-in-beautifulsoup-with-no-class
+    # foruth column represents price, and we want the most recent price so select 0th index. alternatively, use select_one
+    details = soup.select("tr.punk-history-row-sold td:nth-of-type(4)")
+    if len(details) == 0:
+        salePrice = np.nan
+    else: 
+        salePrice = details[0].text
 
-# recent offer price
-details = soup.select("tr.punk-history-row-offer td:nth-of-type(4)")[0]
-offerPrice = details.text
+    # recent offer price
+    details = soup.select("tr.punk-history-row-offer td:nth-of-type(4)")
+    if len(details) == 0:
+        offerPrice = np.nan
+    else:
+        offerPrice = details[0].text
 
-# recent bid price
-details = soup.select("tr.punk-history-row-bid td:nth-of-type(4)")[0]
-bidPrice = details.text
-#print(salePrice, offerPrice, bidPrice)
+    # recent bid price
+    details = soup.select("tr.punk-history-row-bid td:nth-of-type(4)")
+    if len(details) == 0:
+        bidPrice = np.nan
+    else:
+        bidPrice = details[0].text
+    #print(salePrice, offerPrice, bidPrice)
 
-# append to lists
-ids.append(id)
-types.append(type)
-attributeCounts.append(attributeCount)
-attributes.append(punkAttributes)
-sales.append(salePrice)
-offers.append(offerPrice)
-bids.append(bidPrice)
+    # append to lists
+    ids.append(id)
+    types.append(type)
+    attributeCounts.append(attributeCount)
+    attributes.append(punkAttributes)
+    sales.append(salePrice)
+    offers.append(offerPrice)
+    bids.append(bidPrice)
 
 # Create Dataframe
 cryptoPunks = pd.DataFrame({'ID': ids,
