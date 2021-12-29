d59ad568075a51ecdce6d7f9802a5e8fe7a94272
fahimahmed0x <cpcheats1991@gmail.com>
2021-12-28 21:30:10 -0500

    Get attributes and prices of recently sold CryptoPunk

--- create-dataset.py
+++ create-dataset.py
@@ -15,6 +15,7 @@ base_url = "https://www.larvalabs.com"
 sale_links = []
 
 # find all cryptopunk links.
+# https://youtu.be/zD0FDYI5_rs
 for link in soup.find_all('a'):
     url = link.get('href')
 
@@ -24,3 +25,42 @@ for link in soup.find_all('a'):
         sale_links.append(url)
 
 #print(len(sale_links))
+
+request = requests.get(base_url + sale_links[0])
+soup = BeautifulSoup(request.text)
+print(soup.title.string)
+
+#details = soup.find(id="punkDetails")
+details = soup.find("div", class_="col-md-10 col-md-offset-1 col-xs-12")
+type = details.h4.a.text
+
+#attributes
+details = soup.find("div", class_="col-md-10 col-md-offset-1")
+attributeCount = details.p.a.text # number of attributes as a String
+attributeCount = attributeCount.split(' ')[0] # get just the number in the sentence
+
+punkAttributes = [] #attributes specific to this CryptoPunk
+#append attributes to a list
+for attribute in soup.find_all('div', class_="col-md-4"):
+    punkAttributes.append(attribute.a.text)
+punkAttributes = punkAttributes[1:] #ignore the 0th index as it's not an attribute.
+
+#checking
+if int(attributeCount) == len(punkAttributes):
+    print('success!')
+
+# most recent sale price
+# https://stackoverflow.com/questions/64542519/getting-first-or-a-specific-td-in-beautifulsoup-with-no-class
+# foruth column represents price, and we want the most recent price so select 0th index. alternatively, use select_one
+details = soup.select("tr.punk-history-row-sold td:nth-of-type(4)")[0]
+salePrice = details.text
+
+# recent offer price
+details = soup.select("tr.punk-history-row-offer td:nth-of-type(4)")[0]
+offerPrice = details.text
+
+# recent bid price
+details = soup.select("tr.punk-history-row-bid td:nth-of-type(4)")[0]
+bidPrice = details.text
+
+#print(salePrice, offerPrice, bidPrice)
